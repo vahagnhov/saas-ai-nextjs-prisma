a20dbb85353efa6b98f10f0bfdb422d4037c6e9b
vahagnhov <vahagn505315v@gmail.com>
2023-10-18 02:02:09 +0400

    Image Generation AI(Open AI)

---
+++ app/api/image/route.ts
@@ -0,0 +1,51 @@
+import {auth} from '@clerk/nextjs'
+import {NextResponse} from 'next/server'
+import OpenAI from 'openai'
+
+const configuration = {
+    apiKey: process.env.OPENAI_API_KEY,
+};
+
+const openai = new OpenAI(configuration);
+
+export async function POST(
+    req: Request
+) {
+
+    try {
+        const {userId} = auth();
+        const body = await req.json();
+        const {prompt, amount = 1, resolution = "512x512"} = body;
+
+        if (!userId) {
+            return new NextResponse('Unauthorized', {status: 401});
+        }
+
+        if (!configuration.apiKey) {
+            return new NextResponse('OpenAI API Key not configured.', {status: 500});
+        }
+
+        if (!prompt) {
+            return new NextResponse("Prompt is required", {status: 400});
+        }
+
+        if (!amount) {
+            return new NextResponse("Amount is required", {status: 400});
+        }
+
+        if (!resolution) {
+            return new NextResponse("Resolution is required", {status: 400});
+        }
+
+        const image = await openai.images.generate({
+            prompt,
+            n: parseInt(amount, 10),
+            size: resolution
+        });
+
+        return NextResponse.json(image.data);
+    } catch (error) {
+        console.log('[CONVERSATION_ERROR]', error);
+        return new NextResponse('Internal Error', {status: 500});
+    }
+}
