ce45bae6845fa6584b8acf10c2cd3075f78896d3
vahagnhov <vahagn505315v@gmail.com>
2023-10-20 19:45:42 +0400

    API limit UI counter

---
+++ components/free-counter.tsx
@@ -0,0 +1,40 @@
+import {Zap} from 'lucide-react'
+import {useEffect, useState} from 'react'
+
+import {MAX_FREE_COUNTS} from '@/constants'
+import {Card, CardContent} from '@/components/ui/card'
+import {Button} from '@/components/ui/button'
+import {Progress} from '@/components/ui/progress'
+
+export const FreeCounter = ({ apiLimitCount = 0 }: {
+    apiLimitCount: number
+}) => {
+    const [mounted, setMounted] = useState(false);
+
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+
+    if (!mounted) {
+        return null;
+    }
+
+    return (
+        <div className="px-3">
+            <Card className="bg-white/10 border-0">
+                <CardContent className="py-6">
+                    <div className="text-center text-sm text-white mb-4 space-y-2">
+                        <p>
+                            {apiLimitCount} / {MAX_FREE_COUNTS} Free Generations
+                        </p>
+                        <Progress className="h-3" value={(apiLimitCount / MAX_FREE_COUNTS) * 100}/>
+                    </div>
+                    <Button variant="premium" className="w-full">
+                        Upgrade
+                        <Zap className="w-4 h-4 ml-2 fill-white"/>
+                    </Button>
+                </CardContent>
+            </Card>
+        </div>
+    )
+}
